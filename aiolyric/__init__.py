94a12e0ce62d46a021431dadc5b2b1c8e4936445
Keith Lamprecht <1894492+Nixon506E@users.noreply.github.com>
2025-06-27 11:59:12 -0400

    Update __init__.py

--- aiolyric/__init__.py
+++ aiolyric/__init__.py
@@ -102,12 +102,12 @@ class Lyric:
         self.logger.debug(json)
 
         priority = LyricPriority(json)
-        
+
         # device id in the priority payload refers to the mac address of the device
         mac_id = priority.device_id
 
         self._devices_dict[mac_id].current_priority = priority
-        
+
         self._rooms_dict[mac_id] = {}
 
         # add each room to the room dictionary. Rooms contain motion, temp, and humidity averages for all accessories in a room
